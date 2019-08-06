99a8f867b5ce9db91a230f81a41e05acf7636b2c
Vitta <vitta@mayadata.io>
2019-08-06 14:47:23 +0530

    initial rust
    Signed-off-by: Vitta <vitta@mayadata.io>

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+
+fn main() {
+    println!("Hello, world!");
+//    extern crate libzfs;
+
+//    let mut libzfs_handle = libzfs::Libzfs::new();
+//    let pool = libzfs_handle.pool_by_name("test");
+//    let pool = pool.unwrap();
+//    let state = pool.state();
+//    dbg!(pool);
+}
