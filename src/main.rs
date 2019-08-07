c860480c403a18e37d8faae1ec6d245992f9264f
Vishnu Itta <vitta@mayadata.io>
2019-08-07 11:14:21 +0530

    using libzfs to create volume
    Signed-off-by: Vishnu Itta <vitta@mayadata.io>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-
+use libzfs_rs::zfs::{LibZfs};
 
 fn main() {
     println!("Hello, world!");
-//    extern crate libzfs;
 
-//    let mut libzfs_handle = libzfs::Libzfs::new();
+    let mut libzfs_handle = LibZfs::new().unwrap();
 //    let pool = libzfs_handle.pool_by_name("test");
 //    let pool = pool.unwrap();
 //    let state = pool.state();
 //    dbg!(pool);
+
+    let result = libzfs_handle.create_filesystem("test/abcd").unwrap();
 }
